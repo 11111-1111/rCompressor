417e1b6c78c6029038d4f6193b1c120281f1e39a
11111-1111 <87542547+11111-1111@users.noreply.github.com>
2022-09-03 17:53:31 -0400

    implemented rdbende's azure theme

--- compressor.py
+++ compressor.py
@@ -1,9 +1,30 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter.filedialog import askopenfilename
 
 root = Tk()
 
 root.title("rCompressor v1.0")
 root.geometry('500x600')
+
+big_frame = ttk.Frame(root)
+big_frame.pack(fill="both", expand=True)
+
+# Set the initial theme
+root.tk.call("source", "azure.tcl")
+root.tk.call("set_theme", "light")
+
+def change_theme():
+    # NOTE: The theme's real name is azure-<mode>
+    if root.tk.call("ttk::style", "theme", "use") == "azure-dark":
+        # Set light theme
+        root.tk.call("set_theme", "light")
+    else:
+        # Set dark theme
+        root.tk.call("set_theme", "dark")
+
+# Remember, you have to use ttk widgets
+button = ttk.Button(big_frame, text="Change theme!", command=change_theme)
+button.pack()
 
 root.mainloop()
